4d50cc65c04ff3bf43a11611f68d8afef2602e42
Paulami Dutta <230529792+PaulamiDutta23@users.noreply.github.com>
2026-05-23 22:41:33 +0530

    Fixes mongo url in configuration

--- search-service/main.js
+++ search-service/main.js
@@ -4,7 +4,7 @@ import {createApp} from "./src/app.js";
 
 const main = async () => {
   const mongoClient = new MongoClient(Deno.env.get("HOTEL_MONGO_URI") || "mongodb://localhost:27017");
-  const hotelDb = mongoClient.db("hoteldb");
+  const hotelDb = mongoClient.db("hotel-app");
   const hotels = hotelDb.collection("hotels");
   const redisClient = await connect({ hostname: Deno.env.get("REDIS_HOST") || "127.0.0.1", port:6379});
   const app = createApp(redisClient, hotels);
