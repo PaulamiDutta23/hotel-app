bd68d3db34c4f085a53482ef82e279fb01ad188b
Rahul Vishwakarma <rahulvishwakarma@Rahuls-MacBook-Pro.local>
2026-05-23 10:42:41 +0530

    Adds multiple deploy replica for service

--- booking-service/src/main/java/com/tw/step/hotel/bookingservice/model/HotelProjector.java
+++
@@ -1,6 +0,0 @@
-package com.tw.step.hotel.bookingservice.model;
-
-@FunctionalInterface
-public interface HotelProjector<T> {
-    T project(String id, String name, String city, int availableRooms,double pricePerDay);
-}
